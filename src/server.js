c41e09e324bb7eb513e1fbe8e0d818df8c9d6e0f
Otter <otter@Otterui-MacBookPro.local>
2022-06-09 16:04:49 +0900

    playground

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ import { ApolloServer } from "apollo-server-express";
 import { graphqlUploadExpress } from "graphql-upload";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectedResolver } from "./users/users.utils";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 const PORT = process.env.PORT;
 
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    playground: true,
-    introspection: true,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     context: async ({ req }) => {
       return {
         loggedInUser: await getUser(req.headers.authorization),
